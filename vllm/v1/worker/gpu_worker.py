ba0668437751568eedd8865fbeb4ace213109900
Anunay Yadav <anunayyadav@Anunays-MacBook-Pro.local>
2025-08-18 09:44:13 +0200

    cache-blend enabled

--- vllm/v1/worker/gpu_worker.py
+++ vllm/v1/worker/gpu_worker.py
@@ -199,6 +199,11 @@ class Worker(WorkerBase):
         eep_scale_up = os.environ.get("VLLM_ELASTIC_EP_SCALE_UP_LAUNCH") == "1"
         with self._maybe_get_memory_pool_context(tag="weights"):
             self.model_runner.load_model(eep_scale_up=eep_scale_up)
+        from lmcache.v1.compute.models.utils import VLLMModelTracker
+        from lmcache.integration.vllm.utils import ENGINE_NAME
+                
+        VLLMModelTracker.register_model(ENGINE_NAME, self.model_runner.model)
+        ensure_kv_transfer_initialized(self.vllm_config)
 
     def update_config(self, overrides: dict[str, Any]) -> None:
         self.model_runner.update_config(overrides)
@@ -581,7 +586,7 @@ def init_worker_distributed_environment(
     ensure_model_parallel_initialized(parallel_config.tensor_parallel_size,
                                       parallel_config.pipeline_parallel_size)
 
-    ensure_kv_transfer_initialized(vllm_config)
+    # ensure_kv_transfer_initialized(vllm_config)
 
 
 def _check_if_gpu_supports_dtype(torch_dtype: torch.dtype):
